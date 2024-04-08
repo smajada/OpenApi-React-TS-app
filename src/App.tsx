88562947a768006700c857f194816d708b6efb67
Sergi Majada <118728080+smajada@users.noreply.github.com>
2024-04-08 09:12:37 +0200

    feat: inserted layout into App instead of HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,72 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Login from './components/pages/Login';
 import './App.css';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+import { Layout, Menu, theme } from 'antd';
+import { Routes, Route, useLocation, NavLink } from 'react-router-dom';
 
 function App() {
+    const location = useLocation(); // Obtiene la ubicación actual
+    const path = location.pathname;
+    const { Header, Content, Footer } = Layout;
+
+    //Theme tokens
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    const items = [
+        {
+            key: 'Posts',
+            label: 'Posts',
+            path: '/',
+        },
+        {
+            key: 'Login',
+            label: 'Login',
+            path: '/login',
+        },
+    ];
+
     return (
         <>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
+            <Layout>
+                <Header style={{ display: 'flex', alignItems: 'center' }}>
+                    <div className='demo-logo'>Posts</div>
+                    <Menu
+                        theme='dark'
+                        mode='horizontal'
+                        defaultSelectedKeys={path === '/login' ? ['Login'] : ['Posts']}
+                        style={{ flex: 1, minWidth: 0 }}
+                    >
+                        {items.map((item) => (
+                            <Menu.Item key={item.key}>
+                                <NavLink to={item.path}>{item.label}</NavLink>
+                            </Menu.Item>
+                        ))}
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 48px' }}>
+                    <div
+                        style={{
+                            background: colorBgContainer,
+                            minHeight: 280,
+                            padding: 24,
+                            marginTop: 24,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        {/* Routes */}
+                        <Routes>
+                            <Route path='/login' element={<LoginPage />} />
+                            <Route path='/' element={<HomePage />} />
+                        </Routes>
+                        {/* End-Routes */}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Application ©{new Date().getFullYear()} Created by Sergi Majada
+                </Footer>
+            </Layout>
         </>
     );
 }
